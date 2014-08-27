59e31c5754b676d16667f71d4dad1357121569c0
Andrei Tognolo <andreitognolo@gmail.com>
2014-08-27 10:26:08 -0300

    file_monitor

--- file_monitor.sh
+++ file_monitor.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
+
+if [ "x$1" == "x" -o "x$2" == "x" ]; then
+        echo "Script usage:"
+        echo "./file-monitor.sh <file> <script.sh>" 
+        exit 1;
+fi;
+
 while true
 do
    ATIME=`stat -c %Z $1`
