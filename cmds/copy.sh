0e9097b2288b18c953e559a81bb05dcd47b8bfcd
Andrei Tognolo <andreitognolo@gmail.com>
2014-09-01 09:40:44 -0300

    native2ascii

---
+++ cmds/copy.sh
@@ -0,0 +1,1 @@
+cp /home/andrei/projs/extranet/branches/testes-integrados/src/main/webapp/scripts/pageTests.js /home/andrei/opt/apache-tomcat-7.0.54/webapps/ifood-extranet/scripts/pageTests.js
---
+++ cmds/mynative2ascii.sh
@@ -0,0 +1,2 @@
+native2ascii $1 > /tmp/mynative2ascii &&
+cat /tmp/mynative2ascii > $1
