bfe5fe2a0196b1c76b3bca9a34624b548cfdbf9a
Andrei Tognolo <andreitognolo@gmail.com>
2015-04-16 23:04:25 -0300

    .

--- interrupcoes-passivas-palestra/dev_fill_example/db/migrate/20141016210446_create_cities.rb
+++ interrupcoes-passivas-palestra/dev_fill_example/db/migrate/20141016210446_create_cities.rb
@@ -6,5 +6,25 @@ class CreateCities < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    rio_claro = City.new
+    rio_claro.name = 'Rio Claro'
+    rio_claro.state = 'SP'
+    rio_claro.save!
+
+    campinas = City.new
+    campinas.name = 'Campinas'
+    campinas.state = 'SP'
+    campinas.save!
+
+    recife = City.new
+    recife.name = 'Recife'
+    recife.state = 'PE'
+    recife.save!
+
+    olinda = City.new
+    olinda.name = 'Olinda'
+    olinda.state = 'PE'
+    olinda.save!
   end
 end
