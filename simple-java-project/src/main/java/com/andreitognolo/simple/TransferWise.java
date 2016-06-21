ce9676d02dd80fa0e4305cecc7735679e4fc8434
andreitognolo <andreitognolo@gmail.com>
2016-06-21 10:22:59 -0300

    Simple Java Project

---
+++ simple-java-project/src/main/java/com/andreitognolo/simple/TransferWise.java
@@ -0,0 +1,8 @@
+package com.andreitognolo.simple;
+
+public class TransferWise {
+
+    public static void main(String args[]) {
+        System.out.println("Testing");
+    }
+}
---
+++ simple-java-project/src/test/java/com/andreitognolo/simple/TransferWiseTest.java
@@ -0,0 +1,14 @@
+package com.andreitognolo.simple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TransferWiseTest {
+
+    @Test
+    public void test() {
+        assertNotNull(new TransferWise());
+    }
+
+}
